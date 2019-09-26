9b04b10a91a524faa57e41796c5fcec6d6ff8221
lxz <lxztju@163.com>
2019-09-26 11:56:03 +0800

    add longestCommonPrefix.py 2019.09.26

--- easy/longestCommonPrefix.py
+++ easy/longestCommonPrefix.py
@@ -5,14 +5,34 @@
 # @github : https://github.com/lxztju
 
 
+# class Solution:
+#     def longestCommonPrefix(self, strs):
+#         if not strs: return ""
+#         ss = list(map(set, zip(*strs)))
+#         res = ""
+#         for i, x in enumerate(ss):
+#             x = list(x)
+#             if len(x) > 1:
+#                 break
+#             res = res + x[0]
+#         return res
+
+
 class Solution:
     def longestCommonPrefix(self, strs):
         if not strs: return ""
-        ss = list(map(set, zip(*strs)))
-        res = ""
-        for i, x in enumerate(ss):
-            x = list(x)
-            if len(x) > 1:
-                break
-            res = res + x[0]
-        return res
+        s = strs[0]
+        if s == "": return ""
+        result = ""
+        for i in range(len(s)):
+            for j in range(len(strs)):
+                try:
+                    if s[i] != strs[j][i]:
+                        result = s[:i]
+                        return result
+
+                except:
+                    result = s[:i]
+                    return result
+        result = s
+        return result
