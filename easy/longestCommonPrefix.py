8d4cb608d1204f1f094bbf0fbafb0a49d6f5db18
lxz <lxztju@163.com>
2019-09-26 16:45:55 +0800

    add isValid.py 2019.09.26

--- easy/longestCommonPrefix.py
+++ easy/longestCommonPrefix.py
@@ -5,6 +5,16 @@
 # @github : https://github.com/lxztju
 
 
+# 编写一个函数来查找字符串数组中的最长公共前缀。
+#
+# 如果不存在公共前缀，返回空字符串 ""。
+#
+# 示例 1:
+#
+# 输入: ["flower","flow","flight"]
+# 输出: "fl"
+
+
 # class Solution:
 #     def longestCommonPrefix(self, strs):
 #         if not strs: return ""
